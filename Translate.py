67f5730fb5c917d39bc45e935ce479da76d2f7eb
Radin-Alavi <157976960+Radin-Alavi@users.noreply.github.com>
2024-11-27 09:39:10 +0330

    Delete Translate.py

--- Translate.py
+++
@@ -1,9 +0,0 @@
-from translate import Translator
-
-origin = input("Enter text:")
-
-dest = input("Enter the destination:")
-translator = Translator(dest)
-result = translator.translate(origin)
-
-print(result)
